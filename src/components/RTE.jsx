313e31e9334aa0ab54127e7b5c635091bf4c031b
Shree4226 <shreesim4226@gmail.com>
2025-08-17 12:47:58 +0530

    Update RTE.jsx

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -13,7 +13,7 @@ export default function RTE({name, control, label, defaultValue =""}) {
     control={control}
     render={({field: {onChange}}) => (
         <Editor
-         apiKey="hs3r3jrnmh043dzuxwknn96my8ky1d2hh3xhkbi9rth8l20r"
+         apiKey ={conf.appwriteTinyDocs} 
         initialValue={defaultValue}
         init={{
             initialValue: defaultValue,
